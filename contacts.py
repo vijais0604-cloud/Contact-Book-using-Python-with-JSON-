2cd728d8562f7bd109dad518d8c47dc2a12050f3
vijais0604-cloud <vijais0604@gmail.com>
2025-12-28 12:58:34 +0530

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -4,7 +4,7 @@ def load_contact():
      with open("contacts.json","r") as f:
          return json.load(f)
     except FileNotFoundError:
-        return [] 
+        return {} 
 def save_contact():
     with open("contacts.json","w") as f:
         json.dump(book,f,indent=4)    
@@ -15,50 +15,48 @@ class contacts:
         self.name=name
     def create(self,number):
         self.num=number
-        for n in book:
-            if n["name"]==self.name or n["number"]==self.num:
-                print("\nContact already exsist")
-                return
-        book.append({"name":self.name,"number":self.num})
-        print(f"\n{self.name}:{self.num}\n")
+        if self.name in book:
+            print("\nContact already exsist")
+            return
+        book[name]=self.num
+        print(f"\n\n{self.name}:{self.num}\n")
         print("\n Contact added\n")
         save_contact()
     def delete(self):
-        for n in book:
-            if n["name"] == self.name:
-               print(f"\n{self.name}:{n["number"]}\n")
-               book.remove(n)
-               print("\nContact deleted\n")
-               save_contact()
-               return
+        if self.name in book:
+            print(f"\n\n{self.name}:{book[name]}\n")
+            book.pop(self.name,None)
+            print("\nContact deleted\n")
+            save_contact()
+            return
         print("\nContact not found\n")     
     def search(self):
-        for n in book:
-            if n["name"] == self.name:
-                print(f"\n{self.name}:{n["number"]}\n")
-                return
+        if self.name in book:
+            print(f"\n\n{self.name}:{book[name]}\n")
+            return
         print("\ncontact not found\n")      
 while(1):           
  print("\n 1) Search contact \n 2) Add contact \n 3) Delete contact \n 4) Exit")
  vin=int(input("\nenter the option:"))
  match vin:
      case 1:
-         name=input("\nEnter name: \n")
+         name=input("\nEnter name: ")
          a=contacts(name)
          a.search()
      case 2:
-         name=input("\nEnter name: \n")
-         number=input("\nEnter number: \n")
+         name=input("\nEnter name: ")
+         number=input("\nEnter number: ")
          a=contacts(name)
          a.create(number)
      case 3:
-         name=input("\nEnter name: \n")
+         name=input("\nEnter name: ")
          a=contacts(name)
          a.delete()
      case 4:
          break    
      case _:
          print("\nInvalid option\n")
+
 
 
 
